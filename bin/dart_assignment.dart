afd928622b53b8548ffb5bfeda3703579123a79b
MrSohan10 <iamsohan11@gmail.com>
2023-08-03 01:16:19 +0600

    Assignment of module 1 has been completed

---
+++ bin/dart_assignment.dart
@@ -0,0 +1,12 @@
+/// Assignment of Module 1.
+
+void main() {
+  const int a = 10;
+  const int b = 3;
+
+  print("Addition: $a+$b = ${a + b}");
+  print("Subtraction: $a-$b = ${a - b}");
+  print("Multiplication: $a*$b = ${a * b}");
+  print("Division: $a/$b = ${a / b}");
+  print("Modulus: $a%$b = ${a % b}");
+}
