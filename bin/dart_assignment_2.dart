73438d85430445977329d892745a0d4b7a3595b5
MrSohan10 <iamsohan11@gmail.com>
2023-08-10 20:45:32 +0600

    Assignment of module 2 has been completed

---
+++ bin/dart_assignment_2.dart
@@ -0,0 +1,20 @@
+/// Assignment of Module 2.
+
+void main() {
+  List<int> studentGrades = [85, 92, 78, 65, 88, 72];
+  int totalMark = 0; // before summation.
+
+  for (int i = 0; i < studentGrades.length; i++) {
+    totalMark = totalMark + studentGrades[i];
+  }
+
+  double average = totalMark / studentGrades.length;
+  int roundedAverage = average.round();
+  print("Student's average grade: $average \nRounded average: $roundedAverage");
+
+  if (average >= 70) {
+    print("Passed");
+  } else {
+    print("Failed");
+  }
+}
